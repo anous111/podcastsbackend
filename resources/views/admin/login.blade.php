f9406d16f8e7ee757652552cd1ea8d91bbdcda9c
Anis Safia <anissafia90@gmail.com>
2022-07-19 14:35:05 +0100

    test
    enctype="multipart/form-data"
    
    Co-Authored-By: jihedhamdi <94629100+jihedhamdi@users.noreply.github.com>

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -22,7 +22,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b>Admin</b> Bitfumes {{ $newpwd }} </a>
+    <a href="../../index2.html"><b>Admin</b> Bitfumes  </a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
